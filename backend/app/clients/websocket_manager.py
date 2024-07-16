4f0fc6818dee4ebc78db45c143d9b38e085cdfcb
y9yk <andrew.yk82@gmail.com>
2024-07-16 23:00:19 +0900

    feat: add clients for websocket

---
+++ backend/app/clients/websocket_manager.py
@@ -0,0 +1,62 @@
+import asyncio
+from typing import Dict, List
+
+from fastapi import WebSocket
+
+
+class WebSocketManager:
+    """Manage websockets"""
+
+    def init_app(self):
+        """Initialize the WebSocketManager class."""
+        self.active_connections: List[WebSocket] = []
+        self.sender_tasks: Dict[WebSocket, asyncio.Task] = {}
+        self.message_queues: Dict[WebSocket, asyncio.Queue] = {}
+
+    async def close(self):
+        """all connections in active_connections are closed."""
+        for connection in self.active_connections:
+            if connection and isinstance(connection, WebSocket):
+                await connection.close()
+
+    async def start_sender(self, websocket: WebSocket):
+        """Start the sender task."""
+        queue = self.message_queues.get(websocket)
+        if not queue:
+            return
+
+        while True:
+            message = await queue.get()
+            if websocket in self.active_connections:
+                try:
+                    if message == "ping":
+                        await websocket.send_text("pong")
+                    else:
+                        await websocket.send_text(message)
+                except:
+                    break
+            else:
+                break
+
+    async def connect(self, websocket: WebSocket):
+        """Connect a websocket."""
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        self.message_queues[websocket] = asyncio.Queue()
+        self.sender_tasks[websocket] = asyncio.create_task(self.start_sender(websocket))
+
+    async def disconnect(self, websocket: WebSocket):
+        """Disconnect a websocket."""
+        if websocket in self.active_connections:
+            self.active_connections.remove(websocket)
+            self.sender_tasks[websocket].cancel()
+            await self.message_queues[websocket].put(None)
+            del self.sender_tasks[websocket]
+            del self.message_queues[websocket]
+
+
+websocket_manager = WebSocketManager()
+
+
+def get_websocket_manager() -> WebSocketManager:
+    return websocket_manager
