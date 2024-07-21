98f3e750e141649206a934a87bcbc0786d9e1a04
y9yk <andrew.yk82@gmail.com>
2024-07-21 13:12:41 +0900

    feat: add blog_poster_service and use it

--- backend/app/modules/blog_poster/router.py
+++ backend/app/modules/blog_poster/router.py
@@ -3,7 +3,7 @@ import json
 from fastapi import APIRouter, Request, WebSocket, WebSocketDisconnect
 
 from backend.app.clients import WebSocketManager
-from backend.app.modules.blog_poster import BlogPosterRequest
+from backend.app.modules.blog_poster import BlogPosterRequest, BlogPosterService
 
 router = APIRouter(
     tags=["BLOG_POSTER"],
@@ -12,6 +12,10 @@ router = APIRouter(
 
 @router.websocket("/ws")
 async def websocket_endpoint(request: Request, websocket: WebSocket):
+    #
+    blog_poster_service = BlogPosterService()
+
+    #
     websocket_manager: WebSocketManager = request.app.state.websocket_manager
     await websocket_manager.connect(websocket=websocket)
     try:
@@ -24,6 +28,10 @@ async def websocket_endpoint(request: Request, websocket: WebSocket):
             blog_poster_request = BlogPosterRequest(**json.loads(data))
 
             # call service
+            blog_poster_service.process(
+                **blog_poster_request.model_dump(),
+                websocket=websocket,
+            )
 
     except WebSocketDisconnect:
         await websocket_manager.disconnect(websocket=websocket)
